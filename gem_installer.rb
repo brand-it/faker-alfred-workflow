6e6de72a34677c9dd631f736b8368de526040580
Brandt Lareau <brandt.lareau@gmail.com>
2018-09-25 14:11:33 -0700

    Made the installer not print the install process

--- gem_installer.rb
+++ gem_installer.rb
@@ -11,23 +11,37 @@ Gem.clear_paths
 
 module GemInstaller
   def install(lib)
-    return false if `gem list #{lib}`.include?(lib)
-    Logger.info("running 'install #{lib} --no-ri --no-rdoc'")
-    Gem::GemRunner.new.run(['install', lib, '--no-ri', '--no-rdoc'])
-    Logger.info("failed to 'install #{lib} --no-ri --no-rdoc'")
-    false
+    suppress_output do
+      return false if `gem list #{lib}`.include?(lib)
+      Logger.info("running 'install #{lib} --no-ri --no-rdoc'")
+      Gem::GemRunner.new.run(['install', lib, '--no-ri', '--no-rdoc'])
+      Logger.info("failed to 'install #{lib} --no-ri --no-rdoc'")
+      false
+    end
   rescue Gem::SystemExitException
     Logger.info("Successfully Installed #{lib}")
     true
   end
 
   def reinstall(lib)
-    return install(lib) unless `gem list #{lib}`.include?(lib)
-    Logger.info("Uninstalling #{lib}")
-    Gem::GemRunner.new.run(['uninstall', lib, '-a'])
-    Logger.info("Failed to uninstall #{lib}")
-    install(lib)
+    suppress_output do
+      return install(lib) unless `gem list #{lib}`.include?(lib)
+      Logger.info("Uninstalling #{lib}")
+      Gem::GemRunner.new.run(['uninstall', lib, '-a'])
+      Logger.info("Failed to uninstall #{lib}")
+      install(lib)
+    end
   rescue StandardError => exception
     Logger.error("Failure #{exception.message}")
+  end
+
+  def suppress_output
+    original_stdout, original_stderr = $stdout.clone, $stderr.clone
+    $stderr.reopen File.new('/dev/null', 'w')
+    $stdout.reopen File.new('/dev/null', 'w')
+    yield
+  ensure
+    $stdout.reopen original_stdout
+    $stderr.reopen original_stderr
   end
 end
